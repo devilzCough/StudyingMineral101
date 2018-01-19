f9bcafd2f3cb997a0601ccd4742829c03819d50b
DevilzCough <devilzcough@gmail.com>
2018-01-20 02:10:26 +0900

    add actionbar & color change overflow menu icon change

--- app/src/main/java/org/androidtown/studyingmineral101/MainActivity.java
+++ app/src/main/java/org/androidtown/studyingmineral101/MainActivity.java
@@ -1,13 +1,42 @@
 package org.androidtown.studyingmineral101;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int curId = item.getItemId();
+        switch(curId) {
+            case R.id.menu_main:
+                Toast.makeText(this, "main selected", Toast.LENGTH_SHORT).show(); break;
+            case R.id.menu_rank:
+                Toast.makeText(this, "rank selected", Toast.LENGTH_SHORT).show(); break;
+            case R.id.menu_settings:
+                Toast.makeText(this, "settings selected", Toast.LENGTH_SHORT).show(); break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
