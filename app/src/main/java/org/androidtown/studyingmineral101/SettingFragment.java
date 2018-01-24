4a4d02654386c17d9df04d65769d30bc40561ac4
DevilzCough <devilzcough@gmail.com>
2018-01-24 16:11:54 +0900

    complete setting & ranking view

--- app/src/main/java/org/androidtown/studyingmineral101/SettingFragment.java
+++ app/src/main/java/org/androidtown/studyingmineral101/SettingFragment.java
@@ -1,10 +1,23 @@
 package org.androidtown.studyingmineral101;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by iseungjin on 2018. 1. 23..
  */
 
 public class SettingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_setting, container, false);
+
+        return rootView;
+    }
 }
